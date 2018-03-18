9474909e61a51dcc0179bb574ab5b2482a006daa
adamicine <36242767+adamicine@users.noreply.github.com>
2018-03-18 16:57:25 +0100

    Update Pascal_triangle.py

--- Pascal_triangle.py
+++ Pascal_triangle.py
@@ -1,25 +1,17 @@
 #Pascal háromszög progi
 
-def fuggi():
-    print("Ez egy fuggveny aminek nincs parametere!")    
-    sz = ""    
-    for i in range(10):
-        sz+= str(i)
-        sz+= " "
-    print sz
-
 def pascal_triangle():
-    i, koz, j, szam = 0, 0, 0, 1
-    sor = input("Kerem a sorok szamat: ")
-    koz = sor
-    for i in range(sor):     
-        for koz in range(sor,i,-1):
-            print " ",
-        szam=1
-        for j in range(i+1):
-            print str(szam)+" ",
-            szam=szam*(i-j)/(j+1)
-        print"\n",
+	i = koz = j = szam = 0
+	print ("Pascal háromszög program")
+	sor = int(input("Kerem a sorok szamat: "))
+	koz = sor
+	for i in range(sor):     
+		for koz in range(sor,i,-1):
+			print (" ",end="")
+		szam = 1
+		for j in range(i+1):
+			print(str(szam) + " ", end = "")
+			szam = szam * (i - j) / (j + 1)
+		print("\n", end = "")
 
-fuggi()
 pascal_triangle()
