36268ecd1d07e0a1d7bfe11efd9e6a341de90eb4
adamicine <36242767+adamicine@users.noreply.github.com>
2018-10-18 22:05:22 +0200

    Update Windows.py

--- Windows.py
+++ Windows.py
@@ -1,11 +1,19 @@
 from tkinter import *
 
 def windows():
-    window = Tk()
-    label = Label(window,text='blue', fg='red')
-    label.pack()
-    button=Button(window, text='OK')
-    button.pack()
-    window.mainloop()
+    try:
+       window = Tk()
+       window.pack()
+       label1 = Label(window,text='red', fg='red')
+       label2 = Label(window, text='blue', fg='blue')
+       label3 = Label(window, text='green', fg='green')
+       label1.pack()
+       label2.pack()
+       label3.pack()
+       button=Button(window, text='OK')
+       button.pack()
+       window.mainloop()
+    except:
+       print("Something went wrong...")
 
 windows()
