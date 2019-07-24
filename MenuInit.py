24e7fda0e133b0f4a3bcbc3e5e36a3a3692ff875
adamicine <36242767+adamicine@users.noreply.github.com>
2019-07-24 10:10:21 +0200

    Update MenuInit.py

--- MenuInit.py
+++ MenuInit.py
@@ -1,14 +1,16 @@
-from tkinter import *
-class MenuInit(Frame):
+import tkinter
+
+
+class MenuInit(tkinter.Frame):
     def __init__(self,master=None):
-        Frame.__init__(self, master, relief=SUNKEN, bd=2)
+        tkinter.Frame.__init__(self, master, relief=tkinter.SUNKEN, bd=2)
 
-        self.menubar = Menu(self)
+        self.menubar = tkinter.Menu(self)
 
-        menu = Menu(self.menubar,tearoff=0)
+        menu = tkinter.Menu(self.menubar,tearoff=0)
         self.menubar.add_cascade(label="File", menu=menu)
         menu.add_command(label="New")
-        menu = Menu(self.menubar, tearoff=0)
+        menu = tkinter.Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="Edit", menu=menu)
         menu.add_command(label="Cut")
         menu.add_command(label="Copy")
@@ -19,14 +21,14 @@ class MenuInit(Frame):
             # master is a toplevel window (Python 1.4/Tkinter 1.63)
             self.master.tk.call(master, "config", "-menu", self.menubar)
 
-        self.canvas = Canvas(self, bg="white", width=400, height=400,
+        self.canvas = tkinter.Canvas(self, bg="white", width=400, height=400,
                              bd=0, highlightthickness=0)
         self.canvas.pack()
 
 
 
 try:
-    root = Tk()
+    root = tkinter.Tk()
     app = MenuInit(root)
     app.pack()
     root.mainloop()
